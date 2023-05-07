3ef67b4413d5adb0b0d5bb57d67c1034bf5a4d7c
dima <melnik.dimoss@gmail.com>
2023-05-07 21:47:36 +0300

    user apply for google

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import functions_framework
+from models.input.in_user_apply import InUserApply
 
 
 @functions_framework.http
 def user_apply(request):
-    return "Hello"
+    return InUserApply(**request.get_json()).dict()
