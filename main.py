95460c77ddeb44a740230c681836998202700929
dima <melnik.dimoss@gmail.com>
2023-05-07 21:49:38 +0300

    add request type

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from flask import Request
 import functions_framework
 from models.input.in_user_apply import InUserApply
 
 
 @functions_framework.http
-def user_apply(request):
+def user_apply(request: Request):
     return InUserApply(**request.get_json()).dict()
