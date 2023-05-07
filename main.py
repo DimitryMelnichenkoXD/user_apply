cf0b7764249e0c2723c246abe3bc3970226adc47
dima <melnik.dimoss@gmail.com>
2023-05-07 22:38:29 +0300

    add request type

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Request
 
 from utilities.converter import image_to_byte
 from utilities.generation_controller import GenerationController
-from google.cloud.functions import HttpResponse
+# from google.cloud.functions import HttpResponse
 
 
 @functions_framework.http
@@ -12,4 +12,5 @@ def user_apply(request: Request):
     image = generation_controller.image_generate()
     content = image_to_byte(image)
     headers = {"Content-Disposition": "attachment; filename={}".format("generated_image.png")}
-    return HttpResponse(content, headers=headers)
+    # return HttpResponse(content, headers=headers)
+    return "Dima"
